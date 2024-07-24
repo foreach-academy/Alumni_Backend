ea815a758d612a0d36205b5ea78ce329fe670100
aVALKY <Fillierelouis1@gmail.com>
2024-07-24 16:05:56 +0200

    Sécurisation de connexion

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require ("express");
 const cors = require ("cors");
 const app = express();
+const AuthenticateRoutes = require('./Routes/AuthenticateRoute');
+const AuthenticateController = require('./Controllers/AuthenticateControllers');
 
 require('./Config/Associations');
 app.use(cors());
 app.use(express.json());
+app.use('/authenticate', AuthenticateRoutes)
 
 const accountRoutes = require('./Routes/CompteRoute');
 const profilRoutes = require('./Routes/ProfilRoute');
 const utilisateurRoutes = require('./Routes/UtilisateurRoute');
-const authenticateRoutes = require('./Routes/AuthenticateRoute');
 
 app.use('/account', accountRoutes);
-app.use('/profil', profilRoutes);
-app.use('/utilisateur', utilisateurRoutes);
-app.use('/authenticate', authenticateRoutes);
+app.use('/profil', AuthenticateController.authenticateToken, profilRoutes);
+app.use('/utilisateur', AuthenticateController.authenticateToken, utilisateurRoutes);
+app.use('/authenticate', AuthenticateRoutes);
 
-module.exports = app ;
+module.exports = app;
