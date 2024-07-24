79b0a3b3c8f83d426b90b8a6e48e38cd2ecc69d7
Rida Ouajib <rida.ouajib@outlook.fr>
2024-07-24 11:00:31 +0200

    Changement route par /validate/:id et modification dans l'utilisateur service du nom de la colonne

--- Routes/UtilisateurRoute.js
+++ Routes/UtilisateurRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const UtilisateurController = require('../Controllers/UtilisateurController');
 
 router.post('/create', (req, res) => UtilisateurController.addUtilisateur(req, res));
-router.patch('/:id' ,(request, result) => UtilisateurController.validateUser(request,result));
+router.patch('/validate/:id' ,(request, result) => UtilisateurController.validateUser(request,result));
 
 module.exports = router;
