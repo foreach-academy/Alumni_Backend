c01eede70aca4016a82e624c86d465abcb49bc1e
Ryan <ryandaltrey@hotmail.com>
2024-07-25 22:59:34 +0200

    Ajout de la fonctionalité getallformation

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const accountRoutes = require('./Routes/CompteRoute');
 const profilRoutes = require('./Routes/ProfilRoute');
 const utilisateurRoutes = require('./Routes/UtilisateurRoute');
 const authenticateRoutes = require('./Routes/AuthenticateRoute');
+const FormationRoutes = require('./Routes/FormationRoute');
 
 app.use('/account', accountRoutes);
 app.use('/profil', profilRoutes);
 app.use('/utilisateur', utilisateurRoutes);
 app.use('/authenticate', authenticateRoutes);
+app.use('/formation', FormationRoutes);
 
 module.exports = app;
