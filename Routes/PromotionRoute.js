bb962ddc1d76a36bd12935dd1fd860f757a7ca18
Ryan <ryandaltrey@hotmail.com>
2024-07-25 23:33:16 +0200

    Add fonctionalité getAllPromotion.

---
+++ Routes/PromotionRoute.js
@@ -0,0 +1,5 @@
+const PromotionController = require('../Controllers/PromotionControllers');
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => PromotionController.getAllPromotion(req,res));
--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const accountRoutes = require('./Routes/CompteRoute');
 const profilRoutes = require('./Routes/ProfilRoute');
 const utilisateurRoutes = require('./Routes/UtilisateurRoute');
 const authenticateRoutes = require('./Routes/AuthenticateRoute');
+const PromotionRoutes = require('./Routes/PromotionRoute');
 
 app.use('/account', accountRoutes);
 app.use('/profil', profilRoutes);
 app.use('/utilisateur', utilisateurRoutes);
 app.use('/authenticate', authenticateRoutes);
+app.use('/promotion', PromotionRoutes);
 
 module.exports = app;
