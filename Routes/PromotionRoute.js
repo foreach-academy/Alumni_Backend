84b1f01dcef1d10162d04d538498886a7072e44d
STid95 <noemie.contant@symbol-it.fr>
2024-07-31 09:04:32 +0200

    fix(promotion controller) : remove FormationController from PromotionRoute and export router

--- Routes/PromotionRoute.js
+++ Routes/PromotionRoute.js
@@ -4,8 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => PromotionController.getAllPromotion(req,res));
-
 router.get("/:id", (request, result)  => PromotionController.getPromotionByID(request, result));
 
-router.get("/:id", (request, result)  => FormationControllers.getFormationByID(request, result));
-
+module.exports = router;
