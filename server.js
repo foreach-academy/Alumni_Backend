e4a158f22f24a6ce715b8d1be4be369f21543bf5
Ryan <ryandaltrey@hotmail.com>
2024-08-02 17:18:19 +0200

    add modulo export dans routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require ('./index.js')
 
-app.listen(3006, () => {
-    console.log("Le serveur est lancé sur http://127.0.0.1:3006");
+app.listen(3000, () => {
+    console.log("Le serveur est lancé sur http://127.0.0.1:3000");
 })
